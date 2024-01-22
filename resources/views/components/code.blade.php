8c7deb9088586c4e19ac9b6f12894f439318c9f7
Javier Roman Minguez <javierroman@javierroman.com>
2024-01-22 19:33:27 +0100

    Include format for printing

--- resources/views/components/code.blade.php
+++ resources/views/components/code.blade.php
@@ -3,13 +3,20 @@
     'letters'    
 ])
 
-<div class="text-white text-center flex gap-7">
+<div class="text-center flex gap-7 flex-wrap mt-auto">
     @foreach(explode(' ', $message) as $word)
-        <div>
+        <div class="flex gap-x-2">
             @foreach(str_split($word) as $character)
-                <span class="material-symbols-outlined">
+            <div>
+                <span class="material-symbols-outlined !text-4xl">
                     {{ $letters[strtolower($character)] ?? $character }}
                 </span>
+                <div class="bg-gray-200 border print:border-gray-400 h-12 w-12">
+
+                </div>
+
+            </div>
+                
             @endforeach
         </div>
     @endforeach    
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,8 @@
 <x-layout>
-    <section class="px-6">
-        <div class="container mx-auto">
-            <header class="flex justify-center">
-                <h1 class="text-white">Codebreaker</h1>
-            </header>
-            <livewire:create-code />
-        </div>
+    <section class="container mx-auto px-6 h-full flex flex-col">        
+        <header class="flex justify-center print:justify-start">
+            <h1 class="text-white print:text-black text-4xl print:text-2xl">Codebreaker</h1>
+        </header>
+        <livewire:create-code />   
     </section>   
 </x-layout>
