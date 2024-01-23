25c0945789b387ff535451a515d7cdda0887ad81
Javier Roman Minguez <javierroman@javierroman.com>
2024-01-23 20:31:27 +0100

    Disable the print button if there is no message

--- resources/views/livewire/create-code.blade.php
+++ resources/views/livewire/create-code.blade.php
@@ -1,12 +1,14 @@
 <div class="my-8 flex flex-col items-center gap-10 flex-1">
-    <form class="print:hidden">
-        <textarea wire:model.live="message" id="" cols="30" rows="10" class="bg-white/10 text-white py-2 px-3 rounded-xl"></textarea>
+    <form class="print:hidden w-full">
+        <textarea wire:model.live="message" id="" cols="30" rows="10" class="bg-white/10 text-white py-2 px-3 rounded-xl w-full"></textarea>
         <div class="mt-2 flex justify-end">
-            <button @click="window.print()" class="bg-blue-500 rounded-md px-2 py-1">Print</button>
+            <button type="button" @disabled(! $message) @click="window.print()" class="bg-blue-500 rounded-md px-4 py-1 text-sm font-semibold disabled:bg-gray-300">Print</button>
         </div>
     </form>    
 
     <x-code :message="$message" :letters="$this->letters" />  
 
-    <x-legend :letters="$this->letters" /> 
+    @if($message)
+        <x-legend :letters="$this->letters" /> 
+    @endif
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-layout>
-    <section class="container mx-auto px-6 h-full flex flex-col">        
-        <header class="flex justify-center print:justify-start">
-            <h1 class="text-white print:text-black text-4xl print:text-2xl">Codebreaker</h1>
+    <section class="container mx-auto px-6 h-full flex flex-col" style="max-width: 1000px">        
+        <header class="flex">
+            <h1 class="text-white print:text-black text-3xl print:text-2xl">Codebreaker</h1>
         </header>
         <livewire:create-code />   
     </section>   
